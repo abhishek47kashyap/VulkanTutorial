bcfa83080e91d07148e8cc3fe937ca8eee03aa2e
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2023-02-25 21:57:32 +0900

    upto Vulkan Game Engine Tutorial 05 part 1

--- first_app.cpp
+++ first_app.cpp
@@ -1,12 +1,59 @@
 #include "first_app.hpp"
 
+#include <stdexcept>
+
 namespace lve
 {
+    FirstApp::FirstApp()
+    {
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+
+    FirstApp::~FirstApp()
+    {
+        vkDestroyPipelineLayout(lve_device_.device(), pipeline_layout_, nullptr);
+    }
+
     void FirstApp::run()
     {
         while (!lve_window_.shouldClose())
         {
             glfwPollEvents();
         }
+    }
+
+    void FirstApp::createPipelineLayout()
+    {
+        VkPipelineLayoutCreateInfo info{};
+        info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        info.setLayoutCount = 0;
+        info.pSetLayouts = nullptr;
+        info.pushConstantRangeCount = 0;
+        info.pPushConstantRanges = nullptr;
+
+        if (vkCreatePipelineLayout(lve_device_.device(), &info, nullptr, &pipeline_layout_) != VK_SUCCESS)
+        {
+            throw std::runtime_error("FirstApp::createPipelineLayout(); could not create pipeline layout");
+        }
+    }
+
+    void FirstApp::createPipeline()
+    {
+        auto config = LvePipeline::default_pipeline_config_info_(lve_swap_chain_.width(), lve_swap_chain_.height());
+        config.render_pass = lve_swap_chain_.getRenderPass();
+        config.pipeline_layout = pipeline_layout_;
+        lve_pipeline_ = std::make_unique<LvePipeline>(lve_device_, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", config);
+    }
+
+    void FirstApp::createCommandBuffers()
+    {
+
+    }
+
+    void FirstApp::drawFrame()
+    {
+
     }
 }
--- first_app.hpp
+++ first_app.hpp
@@ -3,6 +3,10 @@
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
 #include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+
+#include <memory>
+#include <vector>
 
 namespace lve
 {
@@ -12,11 +16,26 @@ namespace lve
             static constexpr int WIDTH = 800;
             static constexpr int HEIGHT = 600;
 
+            FirstApp();
+            ~FirstApp();
+
+            // deleting copy operator and copy constructor
+            FirstApp(const FirstApp&) = delete;
+            FirstApp &operator=(const FirstApp&) = delete;
+
             void run();
 
         private:
+            void createPipelineLayout();
+            void createPipeline();
+            void createCommandBuffers();
+            void drawFrame();
+
             LveWindow lve_window_{WIDTH, HEIGHT, "Little Vulkan Engine (lve) project"};
             LveDevice lve_device_{lve_window_};
-            LvePipeline lve_pipeline{lve_device_, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", LvePipeline::default_pipeline_config_info_(WIDTH, HEIGHT)};
+            LveSwapChain lve_swap_chain_{lve_device_, lve_window_.getExtent()};
+            std::unique_ptr<LvePipeline> lve_pipeline_;
+            VkPipelineLayout pipeline_layout_;
+            std::vector<VkCommandBuffer> command_buffers_;
     };
 }
